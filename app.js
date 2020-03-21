0e459cd056ce61455bdd6048cced5c3f76a5d9c2
Matthew Cillis <MattCillis@Matts-MacBook-Pro.local>
2020-03-21 14:09:23 -0400

    got google maps embed to work, but not while using jQuery

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-//google classroom button on HOMEPAGE
-$('#google-classroom-button').click(() => {
-  // console.log('google classroom button clicked')
-  let url = 'https://google.com/classroom';
-  window.open(url);
-});
+// BECAUSE THE HTML PAGE LOADS THE GOOGLE MAPS API BEFORE THE JS FILE RUNS, IT DOESN'T RECOGNIZE THE FUNCTION: initMap(). THAT'S WHY IT HAS TO GO BEFORE THE DOC.READY FUNCTION
 
-$('#units-tab').click(() => {
-  console.log('units tab clicked')
-  // let url = '/units.html';
-  // window.open(url);
-});
-$('#current-events-tab').click(() => {
-  console.log('current events tab clicked')
-  // let url = '/current-events.html';
-  // window.open(url);
-});
+let map;
+function initMap() {
+  let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7078, lng: -73.4444},
+    zoom: 15
 
+  });
+}
+
+$(document).ready(function(){
+  $('')
+
+  //google classroom button on HOMEPAGE
+  $('#google-classroom-button').click(() => {
+    // console.log('google classroom button clicked')
+    let url = 'https://google.com/classroom';
+    window.open(url);
+  });
+
+
+  // Initialize and add the map
+  // async function initMap() {
+  //   // The location of Farmingdale
+  //   const fDale = {lat: 40.7326, lng: 73.4454};
+  //   // The map, centered at Farmingdale
+  //   let url = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDQ3WNxkaa6G6nRIB0lUqdlp4kQl6TG1FU&callback=initMap"
+  //   try {
+  //     let map = await axios.get(url)
+  //       return new google.maps.Map(
+  //         $('.map'), {zoom: 4, center: fDale});
+  //         // The marker, positioned at Farmingdale
+  //         let marker = new google.maps.Marker({position: fDale, map: map});
+  //   } catch(e){
+  //       console.log(e)
+  //   }
+  // }
+  //
+  //
+  // initMap()
+
+})
 //routes
 //database
 //server
