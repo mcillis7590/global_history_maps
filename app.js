b0ce06c02501226b7a343c8087086dfb781a4e28
Matthew Cillis <MattCillis@Matts-MacBook-Pro.local>
2020-03-21 10:21:14 -0400

    simplified pages for now

---
+++ app.js
@@ -0,0 +1,21 @@
+//google classroom button on HOMEPAGE
+$('#google-classroom-button').click(() => {
+  // console.log('google classroom button clicked')
+  let url = 'https://google.com/classroom';
+  window.open(url);
+});
+
+$('#units-tab').click(() => {
+  console.log('units tab clicked')
+  // let url = '/units.html';
+  // window.open(url);
+});
+$('#current-events-tab').click(() => {
+  console.log('current events tab clicked')
+  // let url = '/current-events.html';
+  // window.open(url);
+});
+
+//routes
+//database
+//server
